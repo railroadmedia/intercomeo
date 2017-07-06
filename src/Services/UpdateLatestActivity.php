d4ed3c886391c5daf543713cf34272c190d40657
Jonathan Maingot <jonathan@drumeo.com>
2017-07-06 16:06:32 -0700

    oops forgot one
    (user_id for everything, not email)

--- src/Services/UpdateLatestActivity.php
+++ src/Services/UpdateLatestActivity.php
@@ -18,17 +18,15 @@ class UpdateLatestActivity
     }
 
     /**
-     * @param integer|string $emailOrUserId
+     * @param integer|string $userId
      * @param int $utcTimestamp
      *
-     * The first param must either be a **string** that is the email, or an **integer** that is the user_id
-     *
-     * If the second param is specified it must be a Unix timestamp for the UTC time.
+     * If the second param is specified it must be a Unix timestamp for *the UTC time*.
      */
-    public function store($emailOrUserId, $utcTimestamp = null)
+    public function store($userId, $utcTimestamp = null)
     {
         $this->intercomClient->users->create([
-            is_integer($emailOrUserId) ? 'user_id' : 'email' => $emailOrUserId,
+            'user_id' => $userId,
             'last_request_at' => $utcTimestamp ? $utcTimestamp : time()
         ]);
     }
