8b9def2072875f0c5b1ccdde6a7681476d01218f
Jonathan Maingot <jonathan@drumeo.com>
2017-07-26 10:40:09 -0700

    Failing integration tests for IntercomUsersRepository requirement

---
+++ tests/Integration/IntercomUsersRepository.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jon
+ * Date: 7/26/2017
+ * Time: 9:51 AM
+ */
+
+namespace Railroad\Intercomeo\Tests;
+
+
+class IntercomUsersRepository extends TestCase
+{
+    public function test_store_new_user(){
+        $this->markTestIncomplete();
+    }
+
+    public function test_store_new_user_with_last_request_time_specified(){
+        $this->markTestIncomplete();
+    }
+
+    public function test_store_last_request_time_for_existing_user(){
+        $this->markTestIncomplete();
+    }
+
+    public function test_store_fails_gracefully_if_last_request_input_malformed(){
+        $this->markTestIncomplete();
+    }
+}
