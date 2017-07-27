078a724943d3a2968c25cf5e7569ef9134e3bbf2
Jonathan Maingot <jonathan@drumeo.com>
2017-07-27 10:31:21 -0700

    app id and hmac secret re-added to config

--- config/intercomeo.php
+++ config/intercomeo.php
@@ -6,6 +6,8 @@ return [
     'tables' => [
         'intercom_users' => 'intercom_users',
     ],
+    'app_id' => 'YOUR APP ID HERE',
+    'hmac_secret' => 'YOUR HMAC SECRET HERE',
     'access_token' => 'YOUR ACCESS TOKEN HERE',
     'last_request_buffer_amount' => 1,
     'last_request_buffer_unit' => UserService::$timeUnits['hour'],
