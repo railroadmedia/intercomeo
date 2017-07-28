8990fc5e955371af6d5d72de0a4ac920b164d24e
Jonathan Maingot <jonathan@drumeo.com>
2017-07-28 11:11:55 -0700

    memberAddedTest updated as required by changes to TestCase.
    Namely, users created in *testcases* (methods) rather than in *setUp method of TestCase class*.

--- tests/Functional/MemberAddedTest.php
+++ tests/Functional/MemberAddedTest.php
@@ -6,10 +6,13 @@ class MemberAddedTest extends TestCase
 {
     public function testMemberAddedEvent()
     {
-        $this->markTestIncomplete('broken because of changes to TestCase in commit eb26f16a');
+        $userDetails = $this->generateUserDetails();
+        $userId = $this->getUserIdForGeneratedUser($userDetails);
+        $email = $this->getEmailForGeneratedUser($userDetails);
+        $this->createUser($userId, $email);
 
-//        $user = $this->intercomClient->users->getUsers(['user_id' => $this->userId]);
-//        $this->assertEquals($this->userId, (int) $user->user_id);
-//        $this->assertEquals($this->email, $user->email);
+        $user = $this->intercomClient->users->getUsers(['user_id' => $userId]);
+        $this->assertEquals($userId, (int) $user->user_id);
+        $this->assertEquals($email, $user->email);
     }
 }
